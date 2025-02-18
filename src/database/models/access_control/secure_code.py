ead084b00f515a9b4cf34e5f1841fee5453c1fc9
Alberto Soriano <albertosoriano.e@gmail.com>
2025-02-17 22:13:34 -0500

    fix(secure_code): default uuid4

--- src/database/models/access_control/secure_code.py
+++ src/database/models/access_control/secure_code.py
@@ -15,6 +15,7 @@ class SecureCode(Base):
         UUID(as_uuid=True),
         primary_key=True,
         nullable=False,
+        default=uuid.uuid4,
     )
     user_id: Mapped[uuid.UUID] = mapped_column(
         UUID(as_uuid=True),
